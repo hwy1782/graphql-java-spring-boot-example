888bfe2bfdd16ef1fc5fa4201699ba3db5b2d6b1
hwy <hwy1782@gmail.com>
2018-06-15 13:55:21 +0800

    修改类名

--- src/test/java/com/example/DemoGraphQL/GraphQlApplicationTests.java
+++ src/test/java/com/example/DemoGraphQL/GraphQlApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DemoGraphQlApplicationTests {
+public class GraphQlApplicationTests {
 
 	@Test
 	public void contextLoads() {
